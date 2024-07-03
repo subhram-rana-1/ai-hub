e84bae36771ff5a94349f10865f4729082b78a3f
Subhram Subhrajyoti Rana <Subhram@MacBookPro.local>
2024-07-03 16:40:33 +0530

    import correction

--- action/action_to_fields_mappings.py
+++ action/action_to_fields_mappings.py
@@ -1,4 +1,4 @@
-from api_schemas.enums import ActionType
+from action.api_schemas.enums import ActionType
 
 ACTION_TO_FIELD_MAPPING = {
     ActionType.CLOSE_SLOT: ['experience_name', 'date', 'time'],
